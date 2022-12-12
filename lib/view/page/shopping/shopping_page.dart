10835064462106eacc8a01f0a22e167b0d2d0694
nero <devstory.co.kr@gmail.com>
2022-12-12 19:19:47 +0900

    Add ShoppingPageModel.

--- lib/view/page/shopping/shopping_page.dart
+++ lib/view/page/shopping/shopping_page.dart
@@ -1,14 +1,12 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:tomorrow_house/helper/network_helper.dart';
 import 'package:tomorrow_house/helper/responsive_helper.dart';
 import 'package:tomorrow_house/model/product.dart';
 import 'package:tomorrow_house/service/theme_service.dart';
+import 'package:tomorrow_house/view/base/base_view.dart';
 import 'package:tomorrow_house/view/component/cart_button.dart';
 import 'package:tomorrow_house/view/lang/generated/l10n.dart';
+import 'package:tomorrow_house/view/page/shopping/shopping_page_model.dart';
 import 'package:tomorrow_house/view/page/shopping/widget/product_card.dart';
 import 'package:tomorrow_house/view/page/shopping/widget/setting_bottom_sheet.dart';
 import 'package:tomorrow_house/view/theme/component/button.dart';
@@ -23,133 +21,110 @@ class ShoppingPage extends StatefulWidget {
 }
 
 class _ShoppingPageState extends State<ShoppingPage> {
-  List<Product> productList = [];
-  final TextEditingController textController = TextEditingController();
-  String get keyword => textController.text.trim();
+  final ShoppingPageModel shoppingPageModel = ShoppingPageModel();
 
   @override
   void initState() {
     super.initState();
-    searchProductList();
-  }
-
-  Future<void> searchProductList() async {
-    try {
-      final res = await NetworkHelper.dio.get(
-        'https://gist.githubusercontent.com/nero-angela/d16a5078c7959bf5abf6a9e0f8c2851a/raw/04fb4d21ddd1ba06f0349a890f5e5347d94d677e/ikeaSofaDataIBB.json',
-      );
-
-      setState(() {
-        productList = jsonDecode(res.data).map<Product>((json) {
-          return Product.fromJson(json);
-        }).where((product) {
-          /// 키워드가 비어있는 경우 모두 반환
-          if (keyword.isEmpty) return true;
-
-          /// name이나 brand에 키워드 포함 여부 확인
-          return "${product.name}${product.brand}"
-              .toLowerCase()
-              .contains(keyword.toLowerCase());
-        }).toList();
-      });
-    } catch (e, s) {
-      log('Failed to fetchProductList', error: e, stackTrace: s);
-    }
+    shoppingPageModel.searchProductList();
   }
 
   @override
   Widget build(BuildContext context) {
-    return HideKeyboard(
-      child: Scaffold(
-        appBar: AppBar(
-          titleSpacing: 16,
-          title: Text(
-            S.current.shopping,
-            style: context.font.headline2,
+    return BaseView(
+      viewModel: shoppingPageModel,
+      builder: (context, viewModel) => HideKeyboard(
+        child: Scaffold(
+          appBar: AppBar(
+            titleSpacing: 16,
+            title: Text(
+              S.current.shopping,
+              style: context.font.headline2,
+            ),
+            actions: [
+              Button(
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return const SettingBottomSheet();
+                    },
+                  );
+                },
+                icon: 'option-outline.svg',
+                type: ButtonType.flat,
+              ),
+              const CartButton(),
+            ],
           ),
-          actions: [
-            Button(
-              onPressed: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return const SettingBottomSheet();
-                  },
-                );
-              },
-              icon: 'option-outline.svg',
-              type: ButtonType.flat,
-            ),
-            const CartButton(),
-          ],
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              child: Row(
-                children: [
-                  /// 검색
-                  Expanded(
-                    child: InputField(
-                      controller: textController,
-                      hint: S.current.searchProduct,
-                      onClear: searchProductList,
-                      onSubmitted: (text) => searchProductList(),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: Row(
+                  children: [
+                    /// 검색
+                    Expanded(
+                      child: InputField(
+                        controller: viewModel.textController,
+                        hint: S.current.searchProduct,
+                        onClear: viewModel.searchProductList,
+                        onSubmitted: (text) => viewModel.searchProductList(),
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
+                    const SizedBox(width: 16),
 
-                  /// 검색 버튼
-                  Button(
-                    icon: 'search-outline.svg',
-                    onPressed: searchProductList,
-                  ),
-                ],
+                    /// 검색 버튼
+                    Button(
+                      icon: 'search-outline.svg',
+                      onPressed: viewModel.searchProductList,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            productList.isEmpty
+              viewModel.productList.isEmpty
 
-                /// Empty
-                ? Expanded(
-                    child: Center(
-                      child: Text(
-                        S.current.noProduct,
-                        style: context.font.headline4.copyWith(
-                          fontWeight: context.font.light,
-                          color: context.color.inactive,
+                  /// Empty
+                  ? Expanded(
+                      child: Center(
+                        child: Text(
+                          S.current.noProduct,
+                          style: context.font.headline4.copyWith(
+                            fontWeight: context.font.light,
+                            color: context.color.inactive,
+                          ),
                         ),
                       ),
-                    ),
-                  )
+                    )
 
-                /// ProductCard 목록
-                : Expanded(
-                    child: MasonryGridView.count(
-                      crossAxisCount: context.responsive(
-                        2,
-                        tablet: 3,
-                        desktop: 4,
-                      ),
-                      mainAxisSpacing: 24,
-                      crossAxisSpacing: 16,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 32,
-                      ),
-                      itemCount: productList.length,
-                      itemBuilder: (context, index) {
-                        Product product = productList[index];
+                  /// ProductCard 목록
+                  : Expanded(
+                      child: MasonryGridView.count(
+                        crossAxisCount: context.responsive(
+                          2,
+                          tablet: 3,
+                          desktop: 4,
+                        ),
+                        mainAxisSpacing: 24,
+                        crossAxisSpacing: 16,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 32,
+                        ),
+                        itemCount: viewModel.productList.length,
+                        itemBuilder: (context, index) {
+                          Product product = viewModel.productList[index];
 
-                        /// ProductCard
-                        return ProductCard(product: product);
-                      },
+                          /// ProductCard
+                          return ProductCard(product: product);
+                        },
+                      ),
                     ),
-                  ),
-          ],
+            ],
+          ),
         ),
       ),
     );
