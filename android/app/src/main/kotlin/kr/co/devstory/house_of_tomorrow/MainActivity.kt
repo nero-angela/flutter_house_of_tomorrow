9b6d0cb5107b3244673eb202ab036c2a72df23d9
nero <devstory.co.kr@gmail.com>
2023-02-14 15:10:25 +0900

    Create project.

---
+++ android/app/src/main/kotlin/kr/co/devstory/house_of_tomorrow/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.devstory.house_of_tomorrow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello House Of Tomorrow"),
+        ),
+      ),
+    );
+  }
+}
