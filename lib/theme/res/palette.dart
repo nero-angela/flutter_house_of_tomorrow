2725cacbf598253236bb2bdf0a2933614cdb6f1d
nero <devstory.co.kr@gmail.com>
2023-02-14 15:12:11 +0900

    theme : Add palette class.

---
+++ lib/theme/res/palette.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class Palette {
+  /// Chromatic color (유채색)
+  static Color green = const Color(0xFF5AC463);
+  static Color red = const Color(0xFFFB432F);
+  static Color yellow = const Color(0xFFFEC85B);
+
+  /// Achromatic color (무채색)
+  static Color white = const Color(0xFFFFFFFF);
+  static Color grey100 = const Color(0xFFFAFAFA);
+  static Color grey150 = const Color(0xFFF5F5F5);
+  static Color grey200 = const Color(0xFFEFEFEF);
+  static Color grey250 = const Color(0xFFE8E8E8);
+  static Color grey300 = const Color(0xFFDFDFDF);
+  static Color grey350 = const Color(0xFFC8C8C8);
+  static Color grey400 = const Color(0xFFB7B7B7);
+  static Color grey500 = const Color(0xFF949494);
+  static Color grey600 = const Color(0xFF777777);
+  static Color grey650 = const Color(0xFF616161);
+  static Color grey700 = const Color(0xFF555555);
+  static Color grey750 = const Color(0xFF3E3E3E);
+  static Color grey770 = const Color(0xFF383838);
+  static Color grey800 = const Color(0xFF2A2A2A);
+  static Color grey850 = const Color(0xFF1F1F1F);
+  static Color grey870 = const Color(0xFF1A1A1A);
+  static Color grey900 = const Color(0xFF111111);
+  static Color black = const Color(0xFF000000);
+}
