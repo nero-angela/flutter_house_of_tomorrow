984aa4407a2a10d13672d8f6f86dc3e35e8e0897
nero <devstory.co.kr@gmail.com>
2023-02-16 12:48:06 +0900

    product : Add ProductView.

---
+++ lib/src/view/product/product_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
+
+class ProductView extends StatelessWidget {
+  const ProductView({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.current.product),
+      ),
+    );
+  }
+}
