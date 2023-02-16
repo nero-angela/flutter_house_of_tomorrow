3451f549f40404538743225ee926e536eb221498
nero <devstory.co.kr@gmail.com>
2023-02-16 16:39:40 +0900

    product : Add ProductDesc.

--- lib/src/view/product/product_view.dart
+++ lib/src/view/product/product_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
 import 'package:house_of_tomorrow/src/view/product/widget/product_color_preview.dart';
+import 'package:house_of_tomorrow/src/view/product/widget/product_desc.dart';
 import 'package:house_of_tomorrow/theme/component/color_picker.dart';
 import 'package:house_of_tomorrow/theme/component/pop_button.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
@@ -46,25 +47,31 @@ class _ProductViewState extends State<ProductView> {
         leading: const PopButton(),
         titleSpacing: 0,
       ),
-      body: Wrap(
-        runSpacing: 32,
-        alignment: WrapAlignment.center,
-        children: [
-          /// ProductColorPreview
-          ProductColorPreview(
-            colorIndex: colorIndex,
-            product: widget.product,
-          ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 32),
+        child: Wrap(
+          runSpacing: 32,
+          alignment: WrapAlignment.center,
+          children: [
+            /// ProductColorPreview
+            ProductColorPreview(
+              colorIndex: colorIndex,
+              product: widget.product,
+            ),
 
-          /// ColorPicker
-          ColorPicker(
-            colorIndex: colorIndex,
-            colorList: widget.product.productColorList.map((e) {
-              return e.color;
-            }).toList(),
-            onColorSelected: onColorIndexChanged,
-          ),
-        ],
+            /// ColorPicker
+            ColorPicker(
+              colorIndex: colorIndex,
+              colorList: widget.product.productColorList.map((e) {
+                return e.color;
+              }).toList(),
+              onColorSelected: onColorIndexChanged,
+            ),
+
+            /// ProductDesc
+            ProductDesc(product: widget.product),
+          ],
+        ),
       ),
     );
   }
