8d5cc85352209a8b23c3a65dd15ddb9f44cb34d5
nero <devstory.co.kr@gmail.com>
2023-02-16 18:11:15 +0900

    cart : Add CartService CartItem.

--- lib/src/service/cart_service.dart
+++ lib/src/service/cart_service.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/model/cart_item.dart';
 
-class CartService with ChangeNotifier {}
+class CartService with ChangeNotifier {
+  /// 상품 목록
+  List<CartItem> cartItemList = const [];
+}
