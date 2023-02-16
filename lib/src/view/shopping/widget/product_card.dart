f4ebcfe4ddc88ff07c8dbe5a5a5ac267d05f773c
nero <devstory.co.kr@gmail.com>
2023-02-16 16:28:42 +0900

    product : Improve ProductCard image.

--- lib/src/view/shopping/widget/product_card.dart
+++ lib/src/view/shopping/widget/product_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
 import 'package:house_of_tomorrow/src/service/theme_service.dart';
@@ -35,11 +36,15 @@ class ProductCard extends StatelessWidget {
           children: [
             /// Image
             if (product.productColorList.isNotEmpty)
-              ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.network(
-                  product.productColorList.first.imageUrl,
-                  fit: BoxFit.cover,
+              AspectRatio(
+                aspectRatio: 1 / 1,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: CachedNetworkImage(
+                    imageUrl: product.productColorList.first.imageUrl,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             const SizedBox(height: 4),
