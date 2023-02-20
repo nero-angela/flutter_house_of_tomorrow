4ef0935c8aaf357edb13f680efcac08d2ba49f80
nero <devstory.co.kr@gmail.com>
2023-02-20 16:34:14 +0900

    mvvm : Implement ShoppingView initState() trigger.

--- lib/src/view/shopping/shopping_view.dart
+++ lib/src/view/shopping/shopping_view.dart
@@ -18,16 +18,18 @@ class ShoppingView extends StatefulWidget {
 }
 
 class _ShoppingViewState extends State<ShoppingView> {
+  final ShoppingViewModel shoppingViewModel = ShoppingViewModel();
+
   @override
   void initState() {
     super.initState();
-    // searchProductList();
+    shoppingViewModel.searchProductList();
   }
 
   @override
   Widget build(BuildContext context) {
     return BaseView(
-      viewModel: ShoppingViewModel(),
+      viewModel: shoppingViewModel,
       builder: (context, viewModel) => HideKeyboard(
         child: Scaffold(
           appBar: AppBar(
