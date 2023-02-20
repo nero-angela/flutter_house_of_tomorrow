fe6087f5af19248616f56c45d74e690b09cc880e
nero <devstory.co.kr@gmail.com>
2023-02-20 17:14:51 +0900

    mvvm : Refact ProductView.

--- lib/src/view/product/product_view.dart
+++ lib/src/view/product/product_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:house_of_tomorrow/src/model/cart_item.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
-import 'package:house_of_tomorrow/src/service/cart_service.dart';
 import 'package:house_of_tomorrow/src/view/base_view.dart';
 import 'package:house_of_tomorrow/src/view/product/product_view_model.dart';
 import 'package:house_of_tomorrow/src/view/product/widget/product_color_preview.dart';
@@ -10,13 +8,12 @@ import 'package:house_of_tomorrow/src/view/product/widget/product_layout.dart';
 import 'package:house_of_tomorrow/theme/component/cart_button.dart';
 import 'package:house_of_tomorrow/theme/component/color_picker.dart';
 import 'package:house_of_tomorrow/theme/component/pop_button.dart';
-import 'package:house_of_tomorrow/theme/component/toast/toast.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 import 'package:provider/provider.dart';
 
 import 'widget/product_bottom_sheet.dart';
 
-class ProductView extends StatefulWidget {
+class ProductView extends StatelessWidget {
   const ProductView({
     super.key,
     required this.product,
@@ -25,47 +22,12 @@ class ProductView extends StatefulWidget {
   final Product product;
 
   @override
-  State<ProductView> createState() => _ProductViewState();
-}
-
-class _ProductViewState extends State<ProductView> {
-  /// 선택한 수량
-  int count = 1;
-
-  /// 선택한 색상
-  int colorIndex = 0;
-
-  /// 수량 업데이트 이벤트 함수
-  void onCountChanged(int newCount) {
-    setState(() {
-      count = newCount;
-    });
-  }
-
-  /// 색상 업데이트 이벤트 함수
-  void onColorIndexChanged(int newColorIndex) {
-    setState(() {
-      colorIndex = newColorIndex;
-    });
-  }
-
-  /// 카트에 상품 추가
-  void onAddToCartPressed() {
-    final CartService cartService = context.read();
-    final CartItem newCartItem = CartItem(
-      colorIndex: colorIndex,
-      count: count,
-      isSelected: true,
-      product: widget.product,
-    );
-    cartService.add(newCartItem);
-    Toast.show(S.current.productAdded(widget.product.name));
-  }
-
-  @override
   Widget build(BuildContext context) {
     return BaseView(
-      viewModel: ProductViewModel(),
+      viewModel: ProductViewModel(
+        product: product,
+        cartService: context.read(),
+      ),
       builder: (context, viewModel) => Scaffold(
         appBar: AppBar(
           title: Text(S.current.product),
@@ -85,31 +47,31 @@ class _ProductViewState extends State<ProductView> {
               children: [
                 /// ProductColorPreview
                 ProductColorPreview(
-                  colorIndex: colorIndex,
-                  product: widget.product,
+                  colorIndex: viewModel.colorIndex,
+                  product: product,
                 ),
 
                 /// ColorPicker
                 ColorPicker(
-                  colorIndex: colorIndex,
-                  colorList: widget.product.productColorList.map((e) {
+                  colorIndex: viewModel.colorIndex,
+                  colorList: product.productColorList.map((e) {
                     return e.color;
                   }).toList(),
-                  onColorSelected: onColorIndexChanged,
+                  onColorSelected: viewModel.onColorIndexChanged,
                 ),
 
                 /// ProductDesc
-                ProductDesc(product: widget.product),
+                ProductDesc(product: product),
               ],
             ),
           ),
 
           /// ProductBottomSheet
           productBottomSheet: ProductBottomSheet(
-            count: count,
-            product: widget.product,
-            onCountChanged: onCountChanged,
-            onAddToCartPressed: onAddToCartPressed,
+            count: viewModel.count,
+            product: product,
+            onCountChanged: viewModel.onCountChanged,
+            onAddToCartPressed: viewModel.onAddToCartPressed,
           ),
         ),
       ),
--- lib/src/view/product/product_view_model.dart
+++ lib/src/view/product/product_view_model.dart
@@ -1,3 +1,48 @@
+import 'package:house_of_tomorrow/src/model/cart_item.dart';
+import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/service/cart_service.dart';
 import 'package:house_of_tomorrow/src/view/base_view_model.dart';
+import 'package:house_of_tomorrow/theme/component/toast/toast.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 
-class ProductViewModel extends BaseViewModel {}
+class ProductViewModel extends BaseViewModel {
+  ProductViewModel({
+    required this.cartService,
+    required this.product,
+  });
+
+  final CartService cartService;
+
+  /// 선택한 상품
+  final Product product;
+
+  /// 선택한 수량
+  int count = 1;
+
+  /// 선택한 색상
+  int colorIndex = 0;
+
+  /// 수량 업데이트 이벤트 함수
+  void onCountChanged(int newCount) {
+    count = newCount;
+    notifyListeners();
+  }
+
+  /// 색상 업데이트 이벤트 함수
+  void onColorIndexChanged(int newColorIndex) {
+    colorIndex = newColorIndex;
+    notifyListeners();
+  }
+
+  /// 카트에 상품 추가
+  void onAddToCartPressed() {
+    final CartItem newCartItem = CartItem(
+      colorIndex: colorIndex,
+      count: count,
+      isSelected: true,
+      product: product,
+    );
+    cartService.add(newCartItem);
+    Toast.show(S.current.productAdded(product.name));
+  }
+}
