d335c1e17c739b8201d516ac7cb33590600b647b
nero <devstory.co.kr@gmail.com>
2023-02-21 15:05:36 +0900

    unit_test : Add ProductRepositoryTest.

---
+++ test/src/repository/product_repository_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:house_of_tomorrow/src/repository/product_repository.dart';
+
+void main() {
+  late ProductRepository productRepository;
+
+  setUp(() {
+    productRepository = ProductRepository();
+  });
+
+  group('ProductRepository', () {
+    group('searchProductList()', () {
+      test('통신 성공시 List<Product>를 반환한다.', () async {});
+
+      test('통신 실패시 빈 배열을 반환한다.', () async {});
+
+      test('이름이나 브랜드에 해당 키워드가 포함된 Product만 반환한다.', () async {});
+    });
+  });
+}
