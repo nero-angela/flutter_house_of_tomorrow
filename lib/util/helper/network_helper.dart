gustjr5059/Flutter_house_of_tomorrow_Inflearn

api : Add NetworkHelper.

lib/util/helper/network_helper.dart
@@ -1,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+abstract class NetworkHelper {
+  static final Dio dio = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          // 요청시
+          log('REQ : [${options.method}] ${options.path}');
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          // 수신시
+          log('RES : [${response.statusCode}] ${response.requestOptions.path}');
+          return handler.next(response);
+        },
+        onError: (DioError e, handler) {
+          // 에러시
+          log('ERR : [${e.response?.statusCode}] ${e.requestOptions.path}');
+          return handler.next(e);
+        },
+      ),
+    );
+}

nero-angela/flutter_house_of_tomorrow

lib/util/helper/network_helper.dart
@@ -1,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+abstract class NetworkHelper {
+  static final Dio dio = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          // 요청시
+          log('REQ : [${options.method}] ${options.path}');
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          // 수신시
+          log('RES : [${response.statusCode}] ${response.requestOptions.path}');
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          // 에러시
+          log('ERR : [${e.response?.statusCode}] ${e.requestOptions.path}');
+          return handler.next(e);
+        },
+      ),
+    );
+}